0b5da520e446bd6588813a290974a41f08355ebb
majumderpm <pm19971118@gmail.com>
2022-11-12 20:49:12 +0530

    home page add

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Home.css';
 import Navbar from '../Navbar/Navbar';
-import boyimg from '../../img/boy.png';
-
+import portimg from '../../img/port_img.png';
+import htmllogo from '../../img/html-5.png';
+import jslogo from '../../img/js-file.png';
+import reactlogo from '../../img/physics.png';
+import aboutimg from '../../img/about.png';
+// import skillimg from '../../svg';
 
 const Home = () => {
     return (
@@ -13,21 +17,81 @@ const Home = () => {
             <div className="banner_part" >
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-6 col-sm-12">
-                            <h5>Hello, I'm</h5>
-                            <h1>James Smith</h1>
-                            <h2>A Creative Designer From New York</h2>
+                        <div className="col-md-6 col-sm-12 content">
+                            {/* <h5>Hello,<br/> I'm</h5> */}
+                            <h1>Hello,<br /> I'm James Smith</h1>
+                            {/* <h2>A Creative Designer From New York</h2> */}
                             <p>I'm creative designer based in New York, and I'm very passionate and dedicated to my work.</p>
                             <div className="b_btn">
-                                <button class="btn btn-sm btn-secondary mybtn" type="button">about me</button>
+                                <button class="btn btn-sm btn-secondary mybtn" type="button">Contact now</button>
                             </div>
                         </div>
                         <div className="col-md-6 col-sm-12">
-                            <img src={boyimg} alt="" />
+                            <div className="portfolio_area">
+                                <img src={portimg} alt="" />
+                                <div className="html_img">
+                                    <img src={htmllogo} alt="" />
+                                </div>
+                                <div className="js_img">
+                                    <img src={jslogo} alt="" />
+                                </div>
+                                <div className="reat_img">
+                                    <img src={reactlogo} alt="" />
+                                </div>
+                            </div>
+
+
                         </div>
                     </div>
                 </div>
             </div>
+
+            {/* banner part end here */}
+
+
+            {/* about part start here */}
+
+            <section className='about_part'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 col-sm-12 about_img">
+                            <div className="aboutArea">
+                                <img src={aboutimg} alt="" />
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-sm-12">
+
+                            <div className="content">
+                                <h2>My clients know, that I always provide top-level services</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In risus augue, facilisis blandit vulputate sed, euismod egestas ex. Ut ac tellus et velit luctus pellentesque.</p>
+                                <div className="b_btn">
+                                    <button class="btn btn-sm btn-secondary mybtn" type="button">download CV</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </section>
+
+
+
+            {/* skils part start here */}
+
+            <section className='skils'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-ld-6 col-sm-12">
+
+                        </div>
+                        <div className="col-ld-6 col-sm-12">
+                            {/* <img src={skillimg} alt="" /> */}
+                        </div>
+                    </div>
+                </div>
+            </section>
+
         </>
     );
 };
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -5,31 +5,33 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <header className='container'>
-        <nav className="navbar navbar-expand-lg bg-light bg-color">
-        <div className="logo">
-            <img src={logo} alt="" />
-        </div>
-        <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
-            <li className='nav-item'>
-                <Link to='/' className='nav-link active'>home</Link>
-         </li>
-            <li className='nav-item'>
-                <Link to='/about' className='nav-link'>about</Link>
-         </li>
-            <li className='nav-item'>
-                <Link to='/services' className='nav-link'>services</Link>
-         </li>
-            <li className='nav-item'>
-                <Link to='/expriences' className='nav-link'>expriences</Link>
-         </li>
-            <li className='nav-item'>
-                <Link to='/contact' className='nav-link'>contact</Link>
-         </li>
-        </ul>
-        <button class="btn btn-sm btn-secondary mybtn" type="button">Download CV</button>
-      </nav>
-      </header>
+        <header className='nav_header'>
+            <div className='container'>
+                <nav className="navbar navbar-expand-lg bg-light bg-color">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                    </div>
+                    <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-link active'>home</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-link'>about</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-link'>services</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/expriences' className='nav-link'>expriences</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/contact' className='nav-link'>contact</Link>
+                        </li>
+                    </ul>
+                    <button class="btn btn-sm btn-secondary mybtn" type="button">Download CV</button>
+                </nav>
+            </div>
+        </header>
     );
 };
 
